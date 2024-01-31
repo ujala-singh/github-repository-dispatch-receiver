26c76b52c3df040ff84d98e7acdeb24af4681602
ujala-singh <ujala2yz@gmail.com>
2024-01-31 23:22:21 +0530

    Update Cleanup Script

--- .github/scripts/cleanup.sh
+++ .github/scripts/cleanup.sh
@@ -5,20 +5,24 @@ REPO_URL="https://github.com/ujala-singh/github-repository-dispatch-receiver.git
 # Get the list of tags
 TAGS=$(git ls-remote --tags $REPO_URL | awk -F/ '{print $NF}')
 
-# Calculate the date 2 days ago
-TWO_DAYS_AGO=$(date -u --date='-2 days' "+%Y-%m-%dT%H:%M:%SZ")
+# Calculate the date 5 days ago
+TWO_DAYS_AGO=$(date -u --date='-5 days' "+%Y-%m-%dT%H:%M:%SZ")
 
 for TAG in $TAGS; do
     # Get the tag date
     TAG_DATE=$(git log -1 --format=%ai $TAG)
-
-    # Compare the tag date with the date 2 days ago
-    if [ "$(date -u --date="$TAG_DATE" "+%Y-%m-%dT%H:%M:%SZ")" \< "$TWO_DAYS_AGO" ]; then
-        echo "Deleting tag: $TAG"
-        #git push --delete origin $TAG
-        #git tag --delete $TAG
+    # Check if the tag name doesn't contain "-base"
+    if [[ ! "$TAG" =~ -base ]]; then
+        # Compare the tag date with the date 5 days ago
+        if [ "$(date -u --date="$TAG_DATE" "+%Y-%m-%dT%H:%M:%SZ")" \< "$TWO_DAYS_AGO" ]; then
+            echo "Deleting tag: $TAG"
+            git push --delete origin $TAG
+            git tag --delete $TAG
+            echo "Deleting the release $TAG"
+            gh release delete "$TAG" --yes
+        fi
     fi
 done
 
 # Push the changes to the remote repository
-#git push origin --tags
+git push origin --tags
