c48eb9538a9bd3dabe743b8c84f2c327caf512c0
ujala-singh <ujala2yz@gmail.com>
2024-02-09 17:22:58 +0530

    Update Scripts and Workflows

--- .github/scripts/check-files.sh
+++ .github/scripts/check-files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 PR_NUMBER="$1"
+EMPTY_FLAG=false
+EMPTY_FIELDS=""
 
 # Function to add comment on PR
 add_comment() {
@@ -10,23 +12,38 @@ add_comment() {
 }
 
 # Check if description.txt is empty
-if [ ! -s "/tmp/description.txt" ]; then
-    echo "description.txt is empty, failing the action..."
-    add_comment "$PR_NUMBER" "Error: Description field is empty."
-    exit 1
+if [ -z "$(cat /tmp/description.txt)" ]; then
+    echo "description.txt is empty"
+    EMPTY_FLAG=true
+    EMPTY_FIELDS+="Description"
 fi
 
 # Check if jira.txt is empty
-if [ ! -s "/tmp/jira.txt" ]; then
-    echo "jira.txt is empty, failing the action..."
-    add_comment "$PR_NUMBER" "Error: Jira Links field is empty."
-    exit 1
+if [ -z "$(cat /tmp/jira.txt)" ]; then
+    echo "jira.txt is empty"
+    EMPTY_FLAG=true
+    if [ -n "$EMPTY_FIELDS" ]; then
+        EMPTY_FIELDS+=", Jira Links"
+    else
+        EMPTY_FIELDS+="Jira Links"
+    fi
 fi
 
 # Check if pr_link.txt is empty
-if [ ! -s "/tmp/pr_link.txt" ]; then
-    echo "pr_link.txt is empty, failing the action..."
-    add_comment "$PR_NUMBER" "Error: PR Links field is empty."
+if [ -z "$(cat /tmp/pr_link.txt)" ]; then
+    echo "pr_link.txt is empty"
+    EMPTY_FLAG=true
+    if [ -n "$EMPTY_FIELDS" ]; then
+        EMPTY_FIELDS+=", PR Links"
+    else
+        EMPTY_FIELDS+="PR Links"
+    fi
+fi
+
+# Check if any file is empty
+if $EMPTY_FLAG; then
+    echo "One or more files are empty, failing the action..."
+    add_comment "$PR_NUMBER" "Error: The following fields are empty: $EMPTY_FIELDS"
     exit 1
 fi
 
